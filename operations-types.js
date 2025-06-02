401d8c0949aac036dfb65feff1afd14c6b5f583c
Shakeel Gafieldien <shakeel.gafieldien@gmail.com>
2025-06-02 12:33:02 +0200

    Add files via upload

---
+++ operations-types.js
@@ -0,0 +1,15 @@
+let number1 = 22
+let number2 = 38
+
+let number3 = String(number1)
+
+let sum = number1 + number2
+
+let product = number1 * number2
+
+console.log(`Your first number is ${number1}.`);
+console.log(`Your second number is ${number2}.`);
+console.log(`Your stringNumber will be ${number3} and it is a ${typeof number3}.`);
+console.log(`The sum of ${number1} and ${number2} will equal ${sum}.`);
+console.log(`The product of ${number1} and ${number2} is equal to ${product}`);
+
